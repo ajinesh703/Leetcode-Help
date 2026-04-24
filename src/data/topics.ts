698a95385603418bb7f9c1e9bd3b0fd828f2f72d
amitsingh775284-rgb <amitsingh775284@gmail.com>
2026-04-24 10:02:40 +0530

    Add Binary Tree Level Order Traversal problem
    Added a new problem for Binary Tree Level Order Traversal with a Python solution.

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -2258,7 +2258,32 @@ export const topics: Topic[] = [
                 root = root.right
             else:
                 return root`
-      }
+      },
+      {
+  id: 'tree-5',
+  title: 'Binary Tree Level Order Traversal',
+  difficulty: 'Medium',
+  leetcodeUrl: 'https://leetcode.com/problems/binary-tree-level-order-traversal/',
+  description: 'Given the root of a binary tree, return the level order traversal of its nodes\' values (i.e., from left to right, level by level).',
+  language: 'python',
+  solution: `class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        result = []
+        queue = deque([root])
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result`
+}
     ]
   },
   {
