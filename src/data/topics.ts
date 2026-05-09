c77bc28e43386b61273a480df406d6e58fc41c24
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-09 22:53:30 +0530

    feat: add string problem str-26

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -993,6 +993,30 @@ export const topics: Topic[] = [
 
         res.append(' '.join(line).ljust(maxWidth))
         return res`
+},
+{
+  id: 'str-26',
+  title: 'Longest Valid Parentheses',
+  difficulty: 'Hard',
+  leetcodeUrl: 'https://leetcode.com/problems/longest-valid-parentheses/',
+  description: 'Find the length of the longest valid parentheses substring.',
+  language: 'python',
+  solution: `class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [-1]
+        res = 0
+
+        for i, ch in enumerate(s):
+            if ch == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    res = max(res, i - stack[-1])
+
+        return res`
 }
     ]
   },
