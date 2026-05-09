576ff9aee48d036f49dc3792a9c11cd65f98e6fa
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-09 22:49:59 +0530

    feat: add string problem str-23

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -914,6 +914,33 @@ export const topics: Topic[] = [
                     dp[i][j] = dp[i-1][j-1]
 
         return dp[len(s)][len(p)]`
+},
+{
+  id: 'str-23',
+  title: 'Decode Ways',
+  difficulty: 'Medium',
+  leetcodeUrl: 'https://leetcode.com/problems/decode-ways/',
+  description: 'Count the number of ways to decode a digit string into letters.',
+  language: 'python',
+  solution: `class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == '0':
+            return 0
+
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, len(s) + 1):
+            one = int(s[i-1])
+            two = int(s[i-2:i])
+
+            if one >= 1:
+                dp[i] += dp[i-1]
+            if 10 <= two <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[len(s)]`
 }
     ]
   },
