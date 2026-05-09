e0dd59e26e732f2fb34f66ddbb71e521986e2be3
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-09 22:51:20 +0530

    feat: add string problem str-24

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -941,6 +941,32 @@ export const topics: Topic[] = [
                 dp[i] += dp[i-2]
 
         return dp[len(s)]`
+},
+{
+  id: 'str-24',
+  title: 'Scramble String',
+  difficulty: 'Hard',
+  leetcodeUrl: 'https://leetcode.com/problems/scramble-string/',
+  description: 'Determine if one string is a scrambled version of another string.',
+  language: 'python',
+  solution: `class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        from functools import lru_cache
+
+        @lru_cache(None)
+        def dp(a, b):
+            if a == b:
+                return True
+            if sorted(a) != sorted(b):
+                return False
+            n = len(a)
+            for i in range(1, n):
+                if (dp(a[:i], b[:i]) and dp(a[i:], b[i:])) or \
+                   (dp(a[:i], b[n-i:]) and dp(a[i:], b[:n-i])):
+                    return True
+            return False
+
+        return dp(s1, s2)`
 }
     ]
   },
