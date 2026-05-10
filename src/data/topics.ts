13056a7bb48fad44877e99748ff0611d46455bf7
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-10 23:52:07 +0530

    feat: add string problem str-32

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -1107,6 +1107,27 @@ export const topics: Topic[] = [
                 return i
 
         return -1`
+},
+{
+  id: 'str-32',
+  title: 'Longest Common Subsequence',
+  difficulty: 'Medium',
+  leetcodeUrl: 'https://leetcode.com/problems/longest-common-subsequence/',
+  description: 'Find the length of the longest common subsequence between two strings.',
+  language: 'python',
+  solution: `class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[m][n]`
 }
     ]
   },
