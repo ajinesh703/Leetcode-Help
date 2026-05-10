ab11e217ca7ae3ca68ed7e18ed235fc65b67e479
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-10 23:47:15 +0530

    feat: add string problem str-28

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -1040,6 +1040,29 @@ export const topics: Topic[] = [
                 num -= val
 
         return res`
+},
+{
+  id: 'str-28',
+  title: 'Roman to Integer',
+  difficulty: 'Easy',
+  leetcodeUrl: 'https://leetcode.com/problems/roman-to-integer/',
+  description: 'Convert a roman numeral string to its integer representation.',
+  language: 'python',
+  solution: `class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        res = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+
+        return res`
 }
     ]
   },
