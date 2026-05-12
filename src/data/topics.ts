cd387078f6cb6e56aa7b44867ffb1c2fa6b19cb2
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-12 20:50:47 +0530

    feat: add string problem str-64

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -1789,6 +1789,35 @@ export const topics: Topic[] = [
                 return False
 
         return True`
+},
+{
+  id: 'str-64',
+  title: 'Longest Turbulent Subarray',
+  difficulty: 'Medium',
+  leetcodeUrl: 'https://leetcode.com/problems/longest-turbulent-subarray/',
+  description: 'Find the length of the longest turbulent subarray where comparisons alternate between greater and less than.',
+  language: 'python',
+  solution: `class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n < 2:
+            return n
+
+        res = 1
+        inc = dec = 1
+
+        for i in range(1, n):
+            if arr[i] > arr[i-1]:
+                inc = dec + 1
+                dec = 1
+            elif arr[i] < arr[i-1]:
+                dec = inc + 1
+                inc = 1
+            else:
+                inc = dec = 1
+            res = max(res, inc, dec)
+
+        return res`
 }
 
     ]
