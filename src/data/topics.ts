a36976b05039e54daff17e6182818caa6b672d56
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-12 10:55:55 +0530

    feat: add string problem str-52

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -1559,6 +1559,32 @@ export const topics: Topic[] = [
             right -= 1
 
         return ''.join(s)`
+},
+{
+  id: 'str-52',
+  title: 'Add Binary',
+  difficulty: 'Easy',
+  leetcodeUrl: 'https://leetcode.com/problems/add-binary/',
+  description: 'Given two binary strings, return their sum as a binary string.',
+  language: 'python',
+  solution: `class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = []
+        carry = 0
+        i, j = len(a) - 1, len(b) - 1
+
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            res.append(str(total % 2))
+            carry = total // 2
+
+        return ''.join(reversed(res))`
 }
 
     ]
