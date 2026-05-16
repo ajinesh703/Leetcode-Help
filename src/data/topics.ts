ad9b48950fa9a7aebb1989e0ee5c030627321fe6
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-16 12:49:45 +0530

    feat: add string problem str-107

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -2623,6 +2623,39 @@ export const topics: Topic[] = [
                 length = 1
 
         return res`
+},
+{
+  id: 'str-107',
+  title: 'Find the Index of the First Occurrence in a String',
+  difficulty: 'Easy',
+  leetcodeUrl: 'https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/',
+  description: 'Find the index of the first occurrence of needle in haystack using KMP algorithm.',
+  language: 'python',
+  solution: `class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        n, m = len(haystack), len(needle)
+        if m == 0:
+            return 0
+
+        lps = [0] * m
+        j = 0
+        for i in range(1, m):
+            while j > 0 and needle[i] != needle[j]:
+                j = lps[j-1]
+            if needle[i] == needle[j]:
+                j += 1
+            lps[i] = j
+
+        j = 0
+        for i in range(n):
+            while j > 0 and haystack[i] != needle[j]:
+                j = lps[j-1]
+            if haystack[i] == needle[j]:
+                j += 1
+            if j == m:
+                return i - m + 1
+
+        return -1`
 }
 
     ]
