b60ecb9f0f80c7462da9fc9918c3d9b6e2035100
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-16 12:57:05 +0530

    feat: add string problem str-114

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -2753,6 +2753,20 @@ export const topics: Topic[] = [
         res.append(word2[j:])
 
         return ''.join(res)`
+},
+{
+  id: 'str-114',
+  title: 'Greatest Common Divisor of Strings',
+  difficulty: 'Easy',
+  leetcodeUrl: 'https://leetcode.com/problems/greatest-common-divisor-of-strings/',
+  description: 'Find the largest string that divides both strings evenly.',
+  language: 'python',
+  solution: `class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        from math import gcd
+        if str1 + str2 != str2 + str1:
+            return ""
+        return str1[:gcd(len(str1), len(str2))]`
 }
 
     ]
