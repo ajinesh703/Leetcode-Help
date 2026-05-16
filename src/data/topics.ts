2b57bfba4b51f5bd9c5ea5afd1388d1342c77d70
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-16 12:55:23 +0530

    feat: add string problem str-113

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -2730,6 +2730,29 @@ export const topics: Topic[] = [
         import re
         nums = re.findall(r'\d+', word)
         return len(set(int(n) for n in nums))`
+},
+{
+  id: 'str-113',
+  title: 'Merge Strings Alternately',
+  difficulty: 'Easy',
+  leetcodeUrl: 'https://leetcode.com/problems/merge-strings-alternately/',
+  description: 'Merge two strings alternately character by character, appending remaining characters at the end.',
+  language: 'python',
+  solution: `class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = []
+        i, j = 0, 0
+
+        while i < len(word1) and j < len(word2):
+            res.append(word1[i])
+            res.append(word2[j])
+            i += 1
+            j += 1
+
+        res.append(word1[i:])
+        res.append(word2[j:])
+
+        return ''.join(res)`
 }
 
     ]
