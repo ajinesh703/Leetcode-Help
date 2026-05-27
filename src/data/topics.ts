289a3574873dc63929909ac61aaaeb1384056a01
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-27 17:09:35 +0530

    feat: add string problem str-178

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -4358,6 +4358,33 @@ export const topics: Topic[] = [
                 return True
 
         return False`
+},
+{
+  id: 'str-178',
+  title: 'Fruit Into Baskets',
+  difficulty: 'Medium',
+  leetcodeUrl: 'https://leetcode.com/problems/fruit-into-baskets/',
+  description: 'Find the maximum number of fruits you can pick using two baskets where each basket holds one type of fruit.',
+  language: 'python',
+  solution: `class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        from collections import defaultdict
+        basket = defaultdict(int)
+        left = 0
+        res = 0
+
+        for right in range(len(fruits)):
+            basket[fruits[right]] += 1
+
+            while len(basket) > 2:
+                basket[fruits[left]] -= 1
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+
+            res = max(res, right - left + 1)
+
+        return res`
 }
 
 
