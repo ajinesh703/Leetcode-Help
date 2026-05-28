08bec22fb0142e8d3ea9560ec4e8512f9fb5568d
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-05-28 17:14:54 +0530

    feat: add string problem str-189

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -4615,6 +4615,33 @@ export const topics: Topic[] = [
                 break
 
         return res`
+},
+{
+  id: 'str-189',
+  title: 'Sliding Window Maximum',
+  difficulty: 'Hard',
+  leetcodeUrl: 'https://leetcode.com/problems/sliding-window-maximum/',
+  description: 'Find the maximum value in each sliding window of size k using a monotonic deque.',
+  language: 'python',
+  solution: `class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        from collections import deque
+        dq = deque()
+        res = []
+
+        for i in range(len(nums)):
+            while dq and dq[0] < i - k + 1:
+                dq.popleft()
+
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+
+            dq.append(i)
+
+            if i >= k - 1:
+                res.append(nums[dq[0]])
+
+        return res`
 }
 
 
