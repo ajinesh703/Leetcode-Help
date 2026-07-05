e1743a09b096868fff79bc9750f04efc301d0105
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-07-05 19:43:37 +0530

    feat: add companies to tp-70

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -1581,6 +1581,7 @@ export const patterns: Pattern[] = [
                 right += 1
             result = max(result, rooms)
         return result`,
+        companies: ['Facebook', 'Amazon', 'Google', 'Microsoft', 'Airbnb']
       },
       {
         id: 'tp-71',
