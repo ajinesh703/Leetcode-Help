7e065d7ba039823a61dc414e3a92655fa5abc066
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-07-20 19:35:32 +0530

    feat: add companies to sw-31

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -2546,6 +2546,7 @@ export const patterns: Pattern[] = [
                 left += 1
             result = max(result, right - left + 1)
         return result`,
+        companies: ['Google', 'Amazon', 'Facebook', 'Bloomberg', 'Palantir']
       },
       {
         id: 'sw-32',
