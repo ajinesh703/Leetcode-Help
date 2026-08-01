a0d7f8212e4609e6cb8349a05435c2fb13d59580
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-08-01 18:55:43 +0530

    99 sliding window company

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -4033,6 +4033,7 @@ export const patterns: Pattern[] = [
                 result = max(result, prefix - sl[idx])
             sl.add(prefix)
         return result`,
+        companies: ['Google', 'Amazon', 'Facebook', 'Microsoft', 'Bloomberg']
       },
       {
         id: 'sw-100',
