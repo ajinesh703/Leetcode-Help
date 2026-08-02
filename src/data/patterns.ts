1ade24bacb4ae2f436621cd22904b6aee99951b0
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-08-03 01:13:34 +0530

    feat: add companies to sw-104

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -4145,6 +4145,7 @@ export const patterns: Pattern[] = [
             right_cost = (prefix[n] - prefix[idx]) - q * (n - idx)
             result.append(left_cost + right_cost)
         return result`,
+        companies: ['Amazon', 'Google', 'Facebook', 'Microsoft', 'Bloomberg']
       },
       {
         id: 'sw-105',
