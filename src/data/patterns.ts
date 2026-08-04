723502fd68a9ee28a4517595346bb677996a452b
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-08-05 00:31:00 +0530

    feat: add companies to bs-3

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -4248,6 +4248,7 @@ export const patterns: Pattern[] = [
             else:
                 right = mid
         return nums[left]`
+        companies: ['Amazon', 'Google', 'Facebook', 'Microsoft', 'Uber']
       },
       {
         id: 'bs-4',
