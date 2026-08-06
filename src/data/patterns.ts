b8ccc057b67e41e8776160a9318921719e56b229
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-08-06 10:13:19 +0530

    feat: add companies to bs-8

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -4350,7 +4350,8 @@ export const patterns: Pattern[] = [
                 left = mid + 1
             else:
                 right = mid - 1
-        return left`
+        return left`,
+        companies: ['Amazon', 'Google', 'Facebook', 'Apple', 'Bloomberg']
       },
       {
         id: 'bs-9',
