862cd9f8ddc8bbc7f8e958893a5f0d21b6a664da
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-08-13 18:54:06 +0530

    new name

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -5204,6 +5204,7 @@ export const patterns: Pattern[] = [
             else:
                 left = mid + 1
         return left`,
+        companies: ['Google', 'Amazon', 'Facebook', 'Microsoft', 'Adobe']
       },
       {
         id: 'bs-43',
