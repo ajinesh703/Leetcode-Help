78b6d664f8c0297cf76ae760bdd07a66f976e7c6
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-08-22 19:49:51 +0530

    feat: add companies to bs-62

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -5711,6 +5711,7 @@ export const patterns: Pattern[] = [
         def val(s: str) -> int:
             return int(s) if s.isdigit() else len(s)
         return max(val(s) for s in strs)`,
+        companies: ['Amazon', 'Google', 'Facebook', 'Apple', 'Samsung']
       },
       {
         id: 'bs-63',
