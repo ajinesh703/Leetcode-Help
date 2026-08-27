f0a145402a91de6127b40fce87fd46304af1ec40
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-08-27 22:41:48 +0530

    feat: add companies to bs-66

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -5808,6 +5808,7 @@ export const patterns: Pattern[] = [
             curr_sum += i
             result += 1
         return result`,
+        companies: ['Amazon', 'Google', 'Facebook', 'Microsoft', 'Uber']
       },
       {
         id: 'bs-67',
