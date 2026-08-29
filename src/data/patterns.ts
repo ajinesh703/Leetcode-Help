cadf65f3006206085d5e4ab7dd75db0bc7e587ae
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-08-29 23:55:21 +0530

    feat: add companies to bs-76

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -6011,6 +6011,7 @@ export const patterns: Pattern[] = [
         from bisect import bisect_right
         idx = bisect_right(self.times, t) - 1
         return self.leaders[idx]`,
+        companies: ['Google', 'Amazon', 'Facebook', 'Bloomberg', 'Uber']
       },
       {
         id: 'bs-77',
