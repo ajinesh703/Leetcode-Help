95307e9ccc02e574154f6cb3ffca2a1f30c66ce6
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-09-03 20:23:45 +0530

    feat: add companies to bs-87

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -6297,6 +6297,7 @@ export const patterns: Pattern[] = [
             if dp[idx][1] + pro > dp[-1][1]:
                 dp.append([end, dp[idx][1] + pro])
         return dp[-1][1]`,
+        companies: ['Google', 'Amazon', 'Facebook', 'Airbnb', 'Uber']
       },
       {
         id: 'bs-88',
