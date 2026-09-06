fa294d7c9c5c97e64350198e382728dd0555ed2d
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-09-06 20:11:03 +0530

    feat: add companies to bs-97

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -6551,6 +6551,7 @@ export const patterns: Pattern[] = [
             cur = max(cur, 2 * p[i] + 1)
             right[i] = cur
         return max(left[i] * right[i + 1] for i in range(n - 1))`,
+        companies: ['Google', 'Amazon', 'Facebook', 'Bloomberg', 'Goldman Sachs']
       },
       {
         id: 'bs-98',
