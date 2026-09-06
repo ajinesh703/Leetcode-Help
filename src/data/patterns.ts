b36e44d53c594d66da39fc36e805d6e162bd99d5
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-09-06 20:12:28 +0530

    feat: add companies to bs-99

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -6593,6 +6593,7 @@ export const patterns: Pattern[] = [
                     dp[max(0, i - 30)] + costs[2]
                 )
         return dp[365]`,
+        companies: ['Amazon', 'Google', 'Facebook', 'Apple', 'Uber']
       },
       {
         id: 'bs-100',
