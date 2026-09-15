dd17e9f497c1a7896b5b5bbb539fd9cc4ec491d9
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-09-16 00:33:50 +0530

    feat: add companies to fsp-26

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -7245,6 +7245,7 @@ export const patterns: Pattern[] = [
         while slow:
             slow.next, prev, slow = prev, slow, slow.next
         return prev`,
+        companies: ['Amazon', 'Google', 'Facebook', 'Microsoft', 'Bloomberg']
       },
       {
         id: 'fsp-27',
