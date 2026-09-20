21249936ca29ea617fbeefd49a36cb3d058056cc
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-09-20 12:47:54 +0530

    feat: add companies to mi-1

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -7531,7 +7531,8 @@ export const patterns: Pattern[] = [
                 merged[-1][1] = max(merged[-1][1], end)
             else:
                 merged.append([start, end])
-        return merged`
+        return merged`,
+        companies: ['Facebook', 'Amazon', 'Google', 'Microsoft', 'Bloomberg']
       },
       {
         id: 'mi-2',
