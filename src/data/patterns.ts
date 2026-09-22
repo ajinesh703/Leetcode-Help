20730e795c122d393c761ea5947013baaf1b9a1a
Ajinesh Pratap Singh <ajineshpratap@gmail.com>
2026-09-22 09:08:14 +0530

    feat: add companies to mi-9

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -7711,7 +7711,8 @@ export const patterns: Pattern[] = [
                 i += 1
             else:
                 j += 1
-        return result`
+        return result`,
+        companies: ['Facebook', 'Amazon', 'Google', 'Bloomberg', 'Airbnb']
       },
       {
         id: 'mi-10',
